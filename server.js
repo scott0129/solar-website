3a4aa452edc6ffe7ce2406781fff4b072bb20755
Scott Kim <scott102938@gmail.com>
2019-12-18 11:08:27 +0000

    gitignore and getting led switching

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const { exec } = require('child_process');
 const app = express();
 
-const server = app.listen(7000, () => {
+const server = app.listen(80, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
 
 app.use(express.urlencoded());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-var lightOn = false;
+var lightOn = true;
+
+// Set gpio pin 9 to output
+exec('gpio mode 9 out');
+exec('gpio write 9 1');
 
 app.post('/send', function(request, response){
-    exec('ls | grep js', (err, stdout, stderr) => {
-    });
+    if (lightOn) {
+        exec('gpio write 9 0');
+    } else {
+        exec('gpio write 9 1');
+    }
+    lightOn = !lightOn;
     response.sendFile(__dirname + "/public/index.html");
 });
